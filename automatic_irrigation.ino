d176ad7867afe443b356d3bfc7ec52526c1ddb77
Asım Yasin ŞİMŞEK <94120023+asimyasinsimsek@users.noreply.github.com>
2023-03-16 01:44:27 +0300

    Update automatic_irrigation.ino

--- automatic_irrigation.ino
+++ automatic_irrigation.ino
@@ -1,21 +1,21 @@
-int sensorPin = A0;    //Sensörün takılacağı pin 
+int sensorPin = A0;    
 int relayPin= 8;
 int veri;
 int esikdeger=300;
 int passive_led = 2;
 int active_led = 3;
 
-//Sensörden okunan verinin tutulacağı değişken
+
 
 void setup() {
-  pinMode(sensorPin, INPUT);        //Sensörün takılacağı pini INPUT olarak ayarlıyoruz.
+  pinMode(sensorPin, INPUT);       
   pinMode(relayPin, OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
   Serial.println(veri);
-  veri = analogRead(sensorPin);    //Sensörden dijital okuma yapıyoruz.
-  if(veri > esikdeger and veri!= 0){            //Sensör verisi eşik değerini geçerse if içerisindeki kodlar uygulanır.
+  veri = analogRead(sensorPin);  
+  if(veri > esikdeger and veri!= 0){           
     digitalWrite(relayPin, HIGH);
     delay(5000);
     digitalWrite(passive_led,LOW);
